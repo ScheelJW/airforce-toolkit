326fada8dfe3b6ba16d91e79404b9b1b371faf2f
ScheelJW <121300338+ScheelJW@users.noreply.github.com>
2025-01-26 00:24:32 +0100

    Update epb-opb-drafter.js

--- pages/epb-opb-drafter.js
+++ pages/epb-opb-drafter.js
@@ -1,1 +1,9 @@
+export default function EPBOPBDrafterPage() {
+  return (
+    <div>
+      <h1>EPB/OPB Drafter</h1>
+      <p>Draft content and receive suggestions to improve performance reports.</p>
+    </div>
+  );
+}
 
