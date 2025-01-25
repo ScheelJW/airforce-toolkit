5b8c3d0488c7b732eeeac86559539c3290243e3b
ScheelJW <121300338+ScheelJW@users.noreply.github.com>
2025-01-26 00:24:49 +0100

    Update news-updates.js

--- pages/news-updates.js
+++ pages/news-updates.js
@@ -1,1 +1,9 @@
+export default function NewsUpdatesPage() {
+  return (
+    <div>
+      <h1>News & Updates</h1>
+      <p>Stay updated with the latest news about the Air Force and Space Force.</p>
+    </div>
+  );
+}
 
