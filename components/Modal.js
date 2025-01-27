7a012464d1235b7e53f22d15c48e81948c493cea
ScheelJW <121300338+ScheelJW@users.noreply.github.com>
2025-01-27 23:08:38 +0100

    Update Modal.js

--- components/Modal.js
+++ components/Modal.js
@@ -1,7 +1,7 @@
 // components/Modal.js
 import React from "react";
 
-export default function Modal({ children, onClose }) {
+export function Modal({ children, onClose }) {
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
       <div className="bg-gray-800 text-white rounded-lg shadow-xl p-6 max-w-md w-full relative">
