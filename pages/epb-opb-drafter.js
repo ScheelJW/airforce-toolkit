8445c489a5f58f98694fcb92983b243e87ba5123
ScheelJW <121300338+ScheelJW@users.noreply.github.com>
2025-01-30 20:54:38 +0100

    Update epb-opb-drafter.js

--- pages/epb-opb-drafter.js
+++ pages/epb-opb-drafter.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Header from "../components/Header";
-import Footer from "../components/Header";
+import Footer from "../components/Footer";
 
 const EPB_OPB_Drafter = () => {
   return (
